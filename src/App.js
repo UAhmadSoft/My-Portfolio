abf8c1d2f6c11dd09bb9ef49d3d04e893cb08c87
U Ahmad <UmadAhmad1928@gmail.com>
2021-03-20 10:56:15 +0500

    Feature: Added basic Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Route, Switch } from 'react-router';
 import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import Work from './Components/Work';
+import Contact from './Components/Contact';
 
 function App() {
-   return <div className='App'></div>;
+   return (
+      <div className='App'>
+         <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/work' component={Work} />
+            <Route exact path='/contact' component={Contact} />
+         </Switch>
+      </div>
+   );
 }
 
 export default App;
