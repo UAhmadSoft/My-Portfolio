7d1fe494c4a6868998fef32737f2ac550ab171dc
U Ahmad <UmadAhmad1928@gmail.com>
2021-03-20 11:03:40 +0500

    Feature : Added theme and basic layout set

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import About from './Components/About';
 import Work from './Components/Work';
 import Contact from './Components/Contact';
 
+import Header from './Reuseables/Header';
+import Footer from './Reuseables/Footer';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './Styles/theme';
+
 function App() {
    return (
       <div className='App'>
-         <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-            <Route exact path='/work' component={Work} />
-            <Route exact path='/contact' component={Contact} />
-         </Switch>
+         <ThemeProvider theme={theme}>
+            <Header />
+            <Switch>
+               <Route exact path='/' component={Home} />
+               <Route exact path='/about' component={About} />
+               <Route exact path='/work' component={Work} />
+               <Route exact path='/contact' component={Contact} />
+            </Switch>
+            <Footer />
+         </ThemeProvider>
       </div>
    );
 }
